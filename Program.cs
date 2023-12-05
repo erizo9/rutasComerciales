311053570ce9ab99adde2f7ccfc815d7099d3fdf
Diego Castilla <a26865@svalero.com>
2023-12-05 12:44:50 +0100

    creando program.cs y menu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using rutasComerciales;
+
+namespace RutasComerciales
+{
+    class Program
+    {
+
+        static string logFilePath = "error_log.txt";
+
+               static void Main()
+        {
+            // Inicializar el registro de errores
+            File.WriteAllText(logFilePath, "Inicio del registro de errores\n");
+
+
+
+            while (true)
+            {
+                Console.WriteLine("1. Iniciar sesión");
+                Console.WriteLine("2. Registrarse");
+                Console.WriteLine("3. Zona pública");
+                Console.WriteLine("4. Salir");
+                string? choice = Console.ReadLine();
+
+                try
+                {
+                    switch (choice)
+                    {
+                        case "1":
+  
+                            break;
+                        case "2":
+              
+                            break;
+                        case "3":
+                   
+                            break;
+                        case "4":
+                     
+                            break;
+             
+                       
+                    }
+                }
+                catch (Exception ex)
+                {
+            
+                }
+            }
+        }
